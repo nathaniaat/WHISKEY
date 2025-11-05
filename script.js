d1120ba1929786178798e56a319fdd9fc24f1101
cnadiaa <c14240030@john.petra.ac.id>
2025-11-05 10:10:10 +0700

    show hide for home, about us, adopt ONLY

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-const navLinks = document.querySelector(".nav-links");
-function onToggleMenu(e) {
-  e.name = e.name === "menu" ? "close" : "menu";
-  navLinks.classList.toggle("top-[9%]");
-}
+$(document).ready(function () {
+  const navLinks = document.querySelector(".nav-links");
+  function onToggleMenu(e) {
+    e.name = e.name === "menu" ? "close" : "menu";
+    navLinks.classList.toggle("top-[9%]");
+  }
+  const transitionDuration = 600;
+
+  $("#homeNav").on("click", function () {
+    $("#aboutUs, #adoptPage, #header").fadeOut(0, function () {
+      $("#header").fadeIn(transitionDuration);
+      $("#aboutUs").fadeIn(transitionDuration);
+      $("#adoptPage").fadeIn(transitionDuration);
+    });
+  });
+
+  $("#aboutNav").on("click", function () {
+    $("#header, #adoptPage").fadeOut(transitionDuration, function () {
+      $("#aboutUs").fadeIn(transitionDuration);
+    });
+  });
+
+  $("#adoptNav").on("click", function () {
+    $("#header, #aboutUs").fadeOut(transitionDuration, function () {
+      $("#adoptPage").fadeIn(transitionDuration);
+    });
+  });
+});
